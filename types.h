0fb4ca8beb0ba22c2f8bd4cfa84f0f0c29480054
Jari Ylimainen <jari@ylimainen.fi>
2012-05-03 22:34:18 +0300

    Changed LCD interface to UART, lot's of USB stuff

---
+++ types.h
@@ -0,0 +1,13 @@
+#define NULL 			0x00
+#define SPEED 			0x01
+#define FUEL 			0x02
+#define FUEL_NEED 		0x03
+#define LAP 			0x04
+#define LAPS_REM 		0x05
+#define POS 			0x06
+#define RPM				0x07
+
+#define LAPTIME 		0x10
+#define DELTA 			0x11
+
+#define GEAR			0x1f
